a03da11899b84c0054e2ac727452719ea1d76fa4
jovandosen <jovandosen1994@gmail.com>
2020-03-23 21:36:01 +0100

    App updated

--- public/index.php
+++ public/index.php
@@ -5,11 +5,24 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new \DI\Container();
+
+AppFactory::setContainer($container);
+
 $app = AppFactory::create();
+
+$app->addRoutingMiddleware();
 
 $app->get('/', function(Request $request, Response $response, $args){
 	$response->getBody()->write('Hello world.');
 	return $response;
 });
+
+$app->get('/test', function(Request $request, Response $response, $args){
+	$response->getBody()->write('Test route');
+	return $response;
+});
+
+$app->addErrorMiddleware(true, true, true);
 
 $app->run();
