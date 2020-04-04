5eddaa00916664bcb64c2cff291554c2ed663fa8
jovandosen <jovandosen1994@gmail.com>
2020-04-04 18:20:23 +0200

    Posts List updated

--- public/js/posts-data.js
+++ public/js/posts-data.js
@@ -11,13 +11,18 @@ function getPosts()
 	$.ajax({
 		url: "/data/posts",
 		method: "GET",
+		beforeSend: function(){
+			$("#posts-data-table tbody").append("<tr><td colspan='3' class='text-center'><div class='spinner-border' role='status'><span class='sr-only'>Loading</span></div></td></tr>");
+		},
 		success: function(response){
+
+			$("#posts-data-table tbody").empty();
 			
 			var posts = JSON.parse(response);
 
 			if(posts){
 				for(var i = 0; i < posts.length; i++){
-					$("#posts-data-table tbody").append("<tr><td>"+posts[i].title+"</td><td>"+posts[i].content+"</td><td>"+posts[i].image+"</td></tr>");
+					$("#posts-data-table tbody").append("<tr><td>"+posts[i].title+"</td><td>"+posts[i].content+"</td><td><img src=images/gallery/"+posts[i].image+" width='50px' height='50px'></td></tr>");
 				}
 			}
 
